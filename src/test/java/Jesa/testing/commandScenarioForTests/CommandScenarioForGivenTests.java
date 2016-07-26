e45cffe1b826f2e9ecfbb9c7d899ad66dbd4cc9e
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2016-07-26 13:22:51 -0700

    unit tests for COmmandScenarioFor

---
+++ src/test/java/Jesa/testing/commandScenarioForTests/CommandScenarioForGivenTests.java
@@ -0,0 +1,52 @@
+package Jesa.testing.commandScenarioForTests;
+
+import Jesa.AggregateRootEntityStub;
+import Jesa.testing.AggregateCommandGivenStateBuilder;
+import Jesa.testing.CommandScenarioFor;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+public class CommandScenarioForGivenTests {
+
+    private AggregateCommandGivenStateBuilder<AggregateRootEntityStub> given(Object... events) {
+        return new CommandScenarioFor<AggregateRootEntityStub>(() -> new AggregateRootEntityStub())
+                .given(new Object[0])
+                .given(events);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenThrowsWhenEventsAreNull() {
+        given(null);
+    }
+
+    @Test
+    public void givenDoesNotReturnNull() {
+        assertNotNull(given(new Object[0]));
+    }
+
+    @Test
+    public void giveReturnsGivenBuilderContinuation() {
+        assertThat(given(new Object[0]), instanceOf(AggregateCommandGivenStateBuilder.class));
+    }
+
+    @Test
+    public void givenReturnsNewInstanceOnEachCall() {
+        assertNotSame(given(new Object[0]), given(new Object[0]));
+    }
+
+    @Test
+    public void givenEventsAreEquivalentOnResultingSpecification() {
+        Object[] events = {new Object(), new Object()};
+        Object[] result =
+                given(events)
+                .when((c) -> {})
+                .then(new Object[0])
+                .build()
+                .givens();
+
+        assertArrayEquals(events, result);
+    }
+}
