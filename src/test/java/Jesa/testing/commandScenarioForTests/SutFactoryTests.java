e93710fc17d77663e01ba010b9c618d07ff5d526
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2016-07-26 13:11:18 -0700

    added unit tests for GivenStateBuilder

--- src/test/java/Jesa/testing/commandScenarioForTests/SutFactoryTests.java
+++ src/test/java/Jesa/testing/commandScenarioForTests/SutFactoryTests.java
@@ -1,12 +1,33 @@
 package Jesa.testing.commandScenarioForTests;
 
+import Jesa.AggregateRootEntityStub;
+import Jesa.core.AggregateRootEntity;
+import Jesa.testing.CommandScenarioFor;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.concurrent.Callable;
+
+import static org.hamcrest.core.Is.is;
 
 public class SutFactoryTests {
 
-    //private final CommandScenarioFor<AggregateRootEntityStub> sut;
+    @Test(expected = IllegalArgumentException.class)
+    public void factoryCannotBeNull() {
+        CommandScenarioFor<AggregateRootEntity> scenarioFor = new CommandScenarioFor((Callable<AggregateRootEntity>) null);
+    }
 
     @Test
-    public void factoryCannotBeNull() {
+    public void factoryIsPartOfBuiltSpecification() throws Exception {
+        AggregateRootEntityStub entity = new AggregateRootEntityStub();
+
+        Callable<AggregateRootEntity> result =
+                new CommandScenarioFor<AggregateRootEntityStub>(() -> entity)
+                        .when(command -> {})
+                        .then(new Object[0])
+                        .build()
+                        .sutFactory();
+
+        Assert.assertThat(result.call(), is(entity));
     }
 }
